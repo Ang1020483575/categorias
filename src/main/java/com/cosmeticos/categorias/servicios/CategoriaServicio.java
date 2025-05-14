f6e8b79e3cfb66f315cef860ae85a0a341ed0c12
Angelica Muñoz <maria1020483@gmail.com>
2025-05-13 20:46:47 -0500

    "se soluciona error en insertar"

--- src/main/java/com/cosmeticos/categorias/servicios/CategoriaServicio.java
+++ src/main/java/com/cosmeticos/categorias/servicios/CategoriaServicio.java
@@ -6,4 +6,5 @@ import java.util.List;
 public interface CategoriaServicio {
     List<Categoria> getTodosCategorias();
     List<Categoria> getTodosCategoriasManual();
+    void insertarCategoria(Categoria cat);
 }
